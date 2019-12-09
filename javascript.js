80720c456545adbc48710e09e4bd8f0c856bb866
Lucas Gonzalez <lucasbertaudgonzalez@gmail.com>
2019-12-09 17:29:17 -0500

    added css styling, began dynamically changing css depending on the time using moment

--- javascript.js
+++ javascript.js
@@ -6,7 +6,7 @@ $(document).ready(function(){
 $(".current-day").text(moment().format("D dddd MMMM"));
 
 
-  //this alert is to be replaced by a function saving info to the page upon refresh//
+  
 
   $(".save-button").click(function(){  //find save button, when clicked on find schedule-input class, 
     $(".schedule-input").each(function(){
@@ -20,6 +20,7 @@ $(".current-day").text(moment().format("D dddd MMMM"));
 
 function loadLocalStorage() //for each input.schedule-input class, get the id out of the class, find the matching value 
 {
+    
  $("input.schedule-input").each(function(){    
         var id = $(this).attr("id");
         var value = localStorage.getItem(id);
@@ -32,20 +33,37 @@ function timeColor(){
     $(".schedule-input").each(function(){
         var id = $(this).attr("id");
         var fieldTime = moment().format(id, "hh:mmA"); // formatting the id fieldTime to make it into a "moment" *converting to a moment
-        var thing = moment().isAfter("2010-03-14");
+        var thing = moment().isAfter(id);
+        var scheduleInput = $(".schedule-input")
+
+
+        if (moment("hh:mmA") >= scheduleInput){
+            scheduleInput.css({"opacity": "50%"})
+        } else {
+            scheduleInput.css({"opacity": "5%"})
+           
+        }
+        
         console.log(thing)
         console.log(fieldTime);
         console.log(moment());
-    });
-};
+        
+        
+
+        console.log(scheduleInput)
+       
+})}
+
+
+
 
 
 //when comparing dates and times, formmating is dumb importante. must compare the exact same string. must be compatable, with each other as well with momentjs library. 
 //read more doc, use if statements for if its true that it is after *apply styling* if else *different style* (CSS) 
 
 
-});
 
+});
 
 
 
